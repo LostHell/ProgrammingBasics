05908d1fece4be2316bf0ad2cdd2e36454b046b2
LostHell <losthell92@gmail.com>
2019-04-01 19:05:36 +0200

    First Coding

---
+++ First Steps In Coding/Nums 1 - 20/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nums1To20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 21; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
